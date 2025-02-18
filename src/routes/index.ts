b60d77aeaae3fd7e3536045caec20ef9822822c0
Luiz Henrique <7henrique18@gmail.com>
2025-02-18 10:46:12 -0300

    feat: Add referral tracking and invite link access route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify";
 import { subscribeToEvent } from "./subscribe-to-event";
+import { accessInviteLinkRoute } from "./access-invite-link";
 
 export const routes = (app: FastifyInstance) => {
   app.register(subscribeToEvent);
+  app.register(accessInviteLinkRoute);
 };
