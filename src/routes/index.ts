d5d95f69a647396699e6953b593865b79a367d63
Luiz Henrique <7henrique18@gmail.com>
2025-02-19 21:13:05 -0300

    feat: Add subscriber invites count ranking endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ import type { FastifyInstance } from "fastify";
 import { subscribeToEventRoute } from "./subscribe-to-event";
 import { accessInviteLinkRoute } from "./access-invite-link";
 import { getSubscriberInviteClicksRoute } from "./get-subscriber-invite-clicks";
+import { getSubscriberInvitesCountRoute } from "./get-subscriber-invites-count";
 
 export const routes = (app: FastifyInstance) => {
   app.register(subscribeToEventRoute);
   app.register(accessInviteLinkRoute);
   app.register(getSubscriberInviteClicksRoute);
+  app.register(getSubscriberInvitesCountRoute);
 };
