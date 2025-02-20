68882095e55b95e7ccddcb171d59f4ee6de4b1a7
Luiz Henrique <7henrique18@gmail.com>
2025-02-19 21:14:29 -0300

    feat: Add subscriber ranking position endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import { subscribeToEventRoute } from "./subscribe-to-event";
 import { accessInviteLinkRoute } from "./access-invite-link";
 import { getSubscriberInviteClicksRoute } from "./get-subscriber-invite-clicks";
 import { getSubscriberInvitesCountRoute } from "./get-subscriber-invites-count";
+import { getSubscriberRankingPositionRoute } from "./get-subscriber-ranking-position";
 
 export const routes = (app: FastifyInstance) => {
   app.register(subscribeToEventRoute);
   app.register(accessInviteLinkRoute);
   app.register(getSubscriberInviteClicksRoute);
   app.register(getSubscriberInvitesCountRoute);
+  app.register(getSubscriberRankingPositionRoute);
 };
